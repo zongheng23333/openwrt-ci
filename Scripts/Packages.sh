c24ec4fd70b8a7381b5fc9e6b560aa6987f16404
zongheng23333 <1954640276@qq.com>
2026-08-06 09:14:20 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -62,6 +62,7 @@ UPDATE_PACKAGE "openclash" "vernesong/OpenClash"  "dev"  "pkg"
 UPDATE_PACKAGE "passwall" "kenzok8/small" "master" "pkg"
 UPDATE_PACKAGE "xray-core"   "kenzok8/small"  "master" "pkg"
 UPDATE_PACKAGE "sing-box"   "kenzok8/small"  "master" "pkg"
+UPDATE_PACKAGE "luci-app-homeproxy"   "kenzok8/small-package"  "main" "pkg"
 
 UPDATE_PACKAGE "passwall2" "kenzok8/small" "master" "pkg"
 
